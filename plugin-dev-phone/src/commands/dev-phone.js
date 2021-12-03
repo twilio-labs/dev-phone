f25f476611c595932feb4be7a8023ef829ee099e
Matthew Gilliard <mgilliard@twilio.com>
2021-12-03 16:16:03 +0000

    🗺️ Project layout
    This commit includes the layout for plugin and UI.
    
    Many more details in `DEVELOPMENT.md`

---
+++ plugin-dev-phone/src/commands/dev-phone.js
@@ -0,0 +1,74 @@
+const { TwilioClientCommand } = require('@twilio/cli-core').baseCommands;
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+
+const reformatTwilioPns = twilioResponse => {
+    return {
+        "phone-numbers": twilioResponse.map(
+            ({ phoneNumber, friendlyName }) => ({ phoneNumber, friendlyName }))
+    }
+}
+
+class DevPhoneServer extends TwilioClientCommand {
+    constructor(argv, config, secureStorage) {
+        super(argv, config, secureStorage);
+
+        this.showHeaders = true;
+        this.latestLogEvents = [];
+    }
+
+    async run() {
+        await super.run();
+
+        const props = this.parseProperties() || {};
+        this.validatePropsAndFlags(props, this.flags);
+
+        process.on('SIGINT', function () {
+            console.log("Caught interrupt signal");
+            process.exit();
+        });
+
+        const app = express();
+        app.use(express.json()); // request body parser
+
+        app.get("/ping", (req, res) => {
+            res.json({pong: true});
+        })
+
+        app.get("/phone-numbers", (req, res) => {
+            this.twilioClient.incomingPhoneNumbers.list()
+                .then(pns => res.json(reformatTwilioPns(pns)));
+        })
+
+        app.post("/send-sms", (req, res) => {
+            this.twilioClient.messages
+                .create({
+                    body: req.body.body,
+                    from: req.body.from,
+                    to: req.body.to
+                })
+                .then(message => res.json({ result: message }));
+        })
+
+        app.listen(PORT, () => {
+            console.log(`Hello 👋 Your local webserver is listening on port ${PORT}`);
+            console.log(`Use ctrl-c to stop it`);
+        });
+    }
+
+    validatePropsAndFlags(props, flags) {
+        // Flags defined below can be validated and used here. Example:
+        // https://github.com/twilio/plugin-debugger/blob/main/src/commands/debugger/logs/list.js#L46-L56
+    }
+
+}
+
+DevPhoneServer.description = `Dev Phone local express server`
+
+// Example of how to define flags and properties:
+// https://github.com/twilio/plugin-debugger/blob/main/src/commands/debugger/logs/list.js#L99-L126
+DevPhoneServer.PropertyFlags = {};
+DevPhoneServer.flags = {};
+
+module.exports = DevPhoneServer;
