7386b5bad53b42225b03f629bb000c46e29dba1e
Evan Brim <ebrim@twilio.com>
2021-12-08 17:08:20 -0500

    Added Paste and redesigned the sms form as an example
    Signed-off-by: Evan Brim <ebrim@twilio.com>

--- plugin-dev-phone-client/src/index.js
+++ plugin-dev-phone-client/src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+import { Theme } from "@twilio-paste/core/theme";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Theme.Provider theme="default">
+      <App />
+    </Theme.Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
