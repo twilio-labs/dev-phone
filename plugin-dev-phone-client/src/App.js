a0bb936f652d25dc5de8b87e0bac8a9cd8008fa2
Evan Brim <ebrim@twilio.com>
2021-12-08 17:39:39 -0500

    Updated PhoneNumberPicker styling
    Signed-off-by: Evan Brim <ebrim@twilio.com>

--- plugin-dev-phone-client/src/App.js
+++ plugin-dev-phone-client/src/App.js
@@ -1,12 +1,21 @@
-import './App.css';
+import "./App.css";
 
-import { useEffect, useState } from 'react';
-import Konami from 'konami'
+import { useEffect, useState } from "react";
+import Konami from "konami";
 
 import PhoneNumberPicker from './components/PhoneNumberPicker'
 import SendSmsForm from './components/SendSmsForm';
 import Caller from './components/Caller';
 
+import {
+  Button,
+  Column,
+  Grid,
+  Input,
+  Label,
+  Stack,
+  TextArea,
+} from "@twilio-paste/core";
 
 const sendSms = (from, to, body) => {
   console.log("Get it sent!");
@@ -16,23 +25,21 @@ const sendSms = (from, to, body) => {
     fetch("/send-sms", {
       method: "POST",
       headers: { "content-type": "application/json" },
-      body: JSON.stringify({ from, to, body })
-    })
-
+      body: JSON.stringify({ from, to, body }),
+    });
   } else {
     console.log("Not sending as some data is missing");
   }
-}
+};
 
 const setupKonamiCode = () => {
   const ninetiesMode = new Konami(() => {
     window.alert("Lets party like it's 1991!");
   });
   ninetiesMode.pattern = "383840403739373949575749";
-}
+};
 
 function App() {
-
   const [devPhonePn, setDevPhonePn] = useState(null);
 
   useEffect(() => {
@@ -44,7 +51,7 @@ function App() {
         if (settings.phoneNumber) {
           setDevPhonePn(settings.phoneNumber);
         }
-      })
+      });
   }, []);
 
   return (
@@ -52,17 +59,15 @@ function App() {
       <header className="App-header">
         <p>HELLO :owlwave:</p>
       </header>
-
-      {devPhonePn ?
-        <div>
-          <SendSmsForm devPhonePn={devPhonePn} sendSms={sendSms} />
-          <Caller devPhonePn={devPhonePn} />
-        </div>
-        :
-        <PhoneNumberPicker setDevPhonePn={setDevPhonePn} />
-      }
-
-
+      <Grid gutter="space30">
+        <Column span={4} offset={4}>
+          {devPhonePn ? (
+            <SendSmsForm devPhonePn={devPhonePn} sendSms={sendSms} />
+          ) : (
+            <PhoneNumberPicker setDevPhonePn={setDevPhonePn} />
+          )}
+        </Column>
+      </Grid>
     </div>
   );
 }
--- plugin-dev-phone-client/src/components/SendSmsForm.js
+++ plugin-dev-phone-client/src/components/SendSmsForm.js
@@ -1,13 +1,5 @@
 import { useState } from "react";
-import {
-  Button,
-  Column,
-  Grid,
-  Input,
-  Label,
-  Stack,
-  TextArea,
-} from "@twilio-paste/core";
+import { Button, Input, Label, Stack, TextArea } from "@twilio-paste/core";
 
 const formatPnForForm = (pn) => `${pn.phoneNumber} [${pn.friendlyName}]`;
 
@@ -20,36 +12,29 @@ function SendSmsForm({ devPhonePn, sendSms }) {
   };
 
   return (
-    <Grid gutter="space30">
-      <Column span={4} offset={4}>
-        <Stack orientation="vertical" spacing="space60">
-          <Label htmlFor="sendSmsFromPn">From</Label>
-          <Input
-            id="sendSmsFromPn"
-            disabled={true}
-            value={formatPnForForm(devPhonePn)}
-          />
+    <Stack orientation="vertical" spacing="space60">
+      <Label htmlFor="sendSmsFromPn">From</Label>
+      <Input
+        id="sendSmsFromPn"
+        disabled={true}
+        value={formatPnForForm(devPhonePn)}
+      />
 
-          <Label htmlFor="sendSmsToPn">To</Label>
-          <Input
-            id="sendSmsToPn"
-            placeholder="E.164 format please"
-            defaultValue={toPn}
-            onChange={(e) => setToPn(e.target.value)}
-          />
+      <Label htmlFor="sendSmsToPn">To</Label>
+      <Input
+        id="sendSmsToPn"
+        placeholder="E.164 format please"
+        defaultValue={toPn}
+        onChange={(e) => setToPn(e.target.value)}
+      />
 
-          <Label htmlFor="sendSmsBody">Message</Label>
-          <TextArea
-            id="sendSmsBody"
-            onChange={(e) => setBody(e.target.value)}
-          />
+      <Label htmlFor="sendSmsBody">Message</Label>
+      <TextArea id="sendSmsBody" onChange={(e) => setBody(e.target.value)} />
 
-          <Button variant="primary" onClick={sendIt}>
-            Send it!
-          </Button>
-        </Stack>
-      </Column>
-    </Grid>
+      <Button variant="primary" onClick={sendIt}>
+        Send it!
+      </Button>
+    </Stack>
   );
 }
 
