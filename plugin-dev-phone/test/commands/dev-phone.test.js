6d56a7d720786b9e62d3dda1e97c7df1e245685f
Elise Shanholtz <eshanholtz@twilio.com>
2021-12-08 18:34:52 -0800

    tests: fix phone number flag tests

--- plugin-dev-phone/test/commands/dev-phone.test.js
+++ plugin-dev-phone/test/commands/dev-phone.test.js
@@ -1,4 +1,4 @@
-const { expect, test } = require('@twilio/cli-test');
+const { expect, test, constants, getFakeSid } = require('@twilio/cli-test');
 const { Config, ConfigData } = require('@twilio/cli-core').services.config;
 const DevPhoneServer = require('../../src/commands/dev-phone');
 
@@ -8,22 +8,32 @@ const testConfig = test
   .twilioFakeProfile(ConfigData)
   .twilioCliEnv(Config);
 
+const fakeNumber = '+188888888';
+const fakeNumberSid = getFakeSid('PN')
+const fakeNumberResource = {
+    sid: fakeNumberSid,
+    phone_number: fakeNumber,
+    friendly_name: 'dev-phone',
+}
+
 describe('dev-phone', () => {
   describe('providing phone number on command line', () => {
 
     const testHelper = (args, responseCode, responseBody) => testConfig
       .nock('https://api.twilio.com', api => {
         // api.get('/2010-04-01/Accounts/AC69a381fe7d9987b8a2e98ee7c63ea2b5/IncomingPhoneNumbers/+188888888.json')
-        api.get(uri => true)
+        api.get(`/2010-04-01/Accounts/${constants.FAKE_ACCOUNT_SID}/IncomingPhoneNumbers/${fakeNumber}.json`)
           .query(true)
+          .reply(responseCode, responseBody);
+        api.get(`/2010-04-01/Accounts/${constants.FAKE_ACCOUNT_SID}/IncomingPhoneNumbers.json`)
+          .query({'PhoneNumber': fakeNumber})
           .reply(responseCode, responseBody);
       })
       .twilioCommand(DevPhoneServer, args);
 
-    testHelper(['--phone-number', '+188888888'], 404, {})
-      .it('should not allow phone numbers outside the user\'s account', ctx => {
-        expect(ctx.stderr).to.contain("not associated with your Twilio account");
-      })
+    testHelper(['--phone-number', fakeNumber], 404, { code: 20404, message: "not associated with your Twilio account"})
+      .catch(err => expect(err.message).to.contain('not associated with your Twilio account'))
+      .it('should not allow phone numbers outside the user\'s account');
 
   });
 
