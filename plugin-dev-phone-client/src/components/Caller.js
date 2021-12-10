8f90aec22f71b9509a59872a4403e306d2d4016c
Elise Shanholtz <eshanholtz@twilio.com>
2021-12-10 09:59:45 -0800

    feat: add force flag for phone number config override

--- plugin-dev-phone-client/src/components/Caller.js
+++ plugin-dev-phone-client/src/components/Caller.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Device } from 'twilio-client';
+import { Device } from '@twilio/voice-sdk';
 import { Button, Input, Stack, Heading, Paragraph, Label } from "@twilio-paste/core";
 import { connect } from 'react-redux'
 
