df27e9b2282196222ab5539b2d024bd2e5ea129a
Evan Brim <ebrim@twilio.com>
2021-12-10 15:45:13 -0500

    Somewhat fixed header
    Signed-off-by: Evan Brim <ebrim@twilio.com>

--- plugin-dev-phone-client/src/App.js
+++ plugin-dev-phone-client/src/App.js
@@ -1,20 +1,20 @@
 import { useEffect } from "react";
 import Konami from "konami";
 import { connect } from "react-redux";
-import { changeNumberInUse, configureNumberInUse } from './actions'
-import PhoneNumberPicker from './components/PhoneNumberPicker'
-import SendSmsForm from './components/SendSmsForm';
-import Caller from './components/Caller';
+import { changeNumberInUse, configureNumberInUse } from "./actions";
+import PhoneNumberPicker from "./components/PhoneNumberPicker";
+import SendSmsForm from "./components/SendSmsForm";
+import Caller from "./components/Caller";
+
+import { Box, Column, Grid, Stack, Text } from "@twilio-paste/core";
 
 import {
-  Box,
-  Column,
-  Flex,
-  Grid,
-  Stack,
-  Heading,
-  Text,
-} from "@twilio-paste/core";
+  MediaObject,
+  MediaBody,
+  MediaFigure,
+} from "@twilio-paste/media-object";
+
+import { LogoTwilioIcon } from "@twilio-paste/icons/esm/LogoTwilioIcon";
 
 const formatPnForForm = (number) => `${number} [${number}]`;
 
@@ -40,41 +40,82 @@ const setupKonamiCode = () => {
   ninetiesMode.pattern = "383840403739373949575749";
 };
 
-function App({ channelData, changeNumberInUse, configureNumberInUse, numberInUse }) {
+function App({
+  channelData,
+  changeNumberInUse,
+  configureNumberInUse,
+  numberInUse,
+}) {
   useEffect(() => {
     setupKonamiCode();
-    if(channelData.phoneNumber) {
-      changeNumberInUse(channelData.phoneNumber)
+    if (channelData.phoneNumber) {
+      changeNumberInUse(channelData.phoneNumber);
     }
   }, [changeNumberInUse, channelData]);
 
   return (
     <Grid padding="space30">
       <Column span={12}>
-        <Flex>
-          <Flex>
-            <Box padding="space40">
-              <Heading as="h1" variant="heading20">Twilio dev-phone</Heading>
+        <Box
+          as="header"
+          display={["none", "none", "flex"]}
+          alignItems="stretch"
+          backgroundColor="colorBackgroundBrandHighlight"
+          borderBottomColor="colorBorderInverseWeaker"
+          borderBottomWidth="borderWidth10"
+          borderBottomStyle="solid"
+          position="sticky"
+          top="0px"
+          zIndex="zIndex80"
+        >
+          <Box
+            display="flex"
+            alignItems="center"
+            paddingRight="space70"
+            paddingLeft="space70"
+            borderRightColor="colorBorderInverseWeaker"
+            borderRightWidth={["borderWidth0", "borderWidth0", "borderWidth10"]}
+            width="100%"
+          >
+            <MediaObject verticalAlign="center">
+              <MediaFigure as="h1" spacing="space40">
+                <LogoTwilioIcon
+                  decorative={false}
+                  title="Description of icon"
+                />
+              </MediaFigure>
+              <MediaBody as="h2">Dev-Phone</MediaBody>
+            </MediaObject>
+          </Box>
+
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            flexDirection="row"
+            paddingTop="space60"
+            paddingRight="space70"
+            paddingBottom="space60"
+            paddingLeft="space70"
+            width="100%"
+          >
+            <Box marginTop="space10">
+              <Stack orientation="horizontal" spacing="space60" alignItems="right">
+                <Text>
+                  Dev Phone Name: {" "}
+                  {channelData ? channelData.devPhoneName : "loading"}
+                </Text>
+                {numberInUse ? (
+                  <Text>
+                    Calling/Messaging From: {formatPnForForm(numberInUse)}
+                  </Text>
+                ) : (
+                  ""
+                )}
+              </Stack>
             </Box>
-          </Flex>
-          <Flex grow>
-            <Box
-              padding="space40"
-              width="100%"
-            >
-              <Text>
-                This is{" "}
-                {channelData ? channelData.devPhoneName : "loading"}
-              </Text>
-              {numberInUse ? (
-                <Text>We are {formatPnForForm(numberInUse)}</Text>
-              ) : (
-                ""
-              )}
-            </Box>
-          </Flex>
-        </Flex>
-        <header></header>
+          </Box>
+        </Box>
       </Column>
       <Column span={8} offset={2}>
         {numberInUse ? (
@@ -92,12 +133,12 @@ function App({ channelData, changeNumberInUse, configureNumberInUse, numberInUse
 
 const mapStateToProps = (state) => ({
   channelData: state.channelData,
-  numberInUse: state.numberInUse ? state.numberInUse.phoneNumber : ''
+  numberInUse: state.numberInUse ? state.numberInUse.phoneNumber : "",
 });
 
 const mapDispatchToProps = (dispatch) => ({
   changeNumberInUse: (number) => dispatch(changeNumberInUse(number)),
-  configureNumberInUse: (number) => dispatch(configureNumberInUse(number))
-})
+  configureNumberInUse: (number) => dispatch(configureNumberInUse(number)),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
