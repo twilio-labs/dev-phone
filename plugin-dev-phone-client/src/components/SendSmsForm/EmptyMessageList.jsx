739c19398afbfd884284c91f393d9601ff219e6a
Ricky Holtz <eholtz@twilio.com>
2022-03-09 21:40:23 +0100

    Small changes to spacing, alignment, etc.

--- plugin-dev-phone-client/src/components/SendSmsForm/EmptyMessageList.jsx
+++ plugin-dev-phone-client/src/components/SendSmsForm/EmptyMessageList.jsx
@@ -1,5 +1,5 @@
 import { Box, Column, Flex, Grid, Heading, Paragraph, Text } from '@twilio-paste/core';
-import SuccessIllustration from '../illustrations/SuccessIllustration';
+import SuccessIllustration from '../Illustrations/SuccessIllustration';
 
 function EmptyMessageList({ devPhoneNumber }) {
   return <Grid gutter="space40">
@@ -7,11 +7,11 @@ function EmptyMessageList({ devPhoneNumber }) {
       <Flex hAlignContent={"center"} >
         <SuccessIllustration />
       </Flex>
-      <Box marginTop="space80">
+      <Box marginY="space80">
         <Heading as="h3" variant="heading20" marginBottom="space0">You are all set</Heading>
-        <Paragraph>
-          Your Dev Phone is ready to be used. Any messages you send or receive will show up here. Enter a destination phone number and start sending your first messages. Alternatively send an SMS from your own device to <Text fontFamily={"fontFamilyCode"}>{devPhoneNumber}</Text>.
-        </Paragraph>
+        <Text as={"span"}>
+          Your Dev Phone is ready to be used. Any messages you send or receive will show up here. Enter a destination phone number and start sending your first messages. Alternatively send an SMS from your own device to <Text as={"span"} display={"inline-block"} fontFamily={"fontFamilyCode"}>{devPhoneNumber}</Text>.
+        </Text>
       </Box>
     </Column>
   </Grid>
