c37ea92426a32f0854185ec1f8eab3ceb8b425ed
Dominik Kundel <dkundel@twilio.com>
2022-03-09 09:26:50 -0800

    feat(ui): revamp message list UI

---
+++ plugin-dev-phone-client/src/components/SendSmsForm/MessageBubble.jsx
@@ -0,0 +1,39 @@
+import { Box, Text, MediaObject, MediaBody, Avatar, MediaFigure } from '@twilio-paste/core';
+import { UserIcon } from '@twilio-paste/icons/esm/UserIcon';
+import { formatDistanceToNow } from 'date-fns';
+
+function MessageBubble({ devPhoneName, message }) {
+  console.dir(message);
+  const isFromDevPhone = message.author === devPhoneName;
+
+  return <Box display="flex" justifyContent={isFromDevPhone ? "flex-end" : 'flex-start'} marginBottom="space30">
+    <MediaObject as="div">
+      {!isFromDevPhone && <MediaFigure as="div" align="start" spacing="space40">
+        <Avatar size="sizeIcon50" name={message.author} icon={UserIcon} />
+      </MediaFigure>}
+      <MediaBody as="div">
+        <Box
+          as="div"
+          color={isFromDevPhone ? "colorTextInverse" : "colorText"}
+          backgroundColor={isFromDevPhone ? "colorBackgroundPrimary" : "colorBackground"}
+          borderRadius="borderRadius20"
+          borderTopRightRadius="borderRadius0"
+          fontSize="fontSize30"
+          lineHeight="lineHeight20"
+          padding="space30"
+          maxWidth="size30"
+        >
+          {message.body}
+        </Box>
+        <Text as="div" color="colorTextWeak" fontSize="fontSize20" textAlign={isFromDevPhone ? "right" : 'left'}>
+          {isFromDevPhone ? 'Your Dev Phone' : message.author} &ndash; {formatDistanceToNow(message.dateCreated, { addSuffix: true })}
+        </Text>
+      </MediaBody>
+      {isFromDevPhone && <MediaFigure as="div" align="end" spacing="space40">
+        <Avatar size="sizeIcon50" name="DevPhone" />
+      </MediaFigure>}
+    </MediaObject>
+  </Box>
+}
+
+export default MessageBubble
