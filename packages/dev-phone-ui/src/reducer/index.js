ea0d8f07194055a7dd8412ddef91e587ed72eb8d
Ricky <eholtz@twilio.com>
2022-03-25 13:31:29 +0100

    Remove rogue console.log

--- packages/dev-phone-ui/src/reducer/index.js
+++ packages/dev-phone-ui/src/reducer/index.js
@@ -39,7 +39,6 @@ export default function reducer(state = initialState, action) {
             return {
                 ...state,
                 callLog: state.callLog.map(call => { 
-                    console.log(call.Sid === action.payload.Sid)
                     return call.Sid === action.payload.Sid ? action.payload : call
                 })
             }
