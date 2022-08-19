ca20db080af67f2da85d6622b2d5498bbf3de151
Ricky Holtz <eholtz@twilio.com>
2022-08-19 13:30:41 +0200

    Make port selection async

--- packages/plugin-dev-phone/src/commands/dev-phone.ts
+++ packages/plugin-dev-phone/src/commands/dev-phone.ts
@@ -322,8 +322,8 @@ class DevPhoneServer extends TwilioClientCommand {
         // Flags defined below can be validated and used here. Example:
         // https://github.com/twilio/plugin-debugger/blob/main/src/commands/debugger/logs/list.js#L46-L56
 
-        this.port = process.env.TWILIO_DEV_PHONE_PORT || await getAvailablePort();
         this.cliSettings.forceMode = flags['force'];
+        this.port = process.env.TWILIO_DEV_PHONE_PORT || await getAvailablePort();
         if (flags['phone-number']) {
             this.pns = await this.twilioClient.incomingPhoneNumbers
                 .list({ phoneNumber: flags['phone-number'] });
