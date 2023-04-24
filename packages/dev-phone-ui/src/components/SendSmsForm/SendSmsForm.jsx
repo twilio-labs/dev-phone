cac2757568337f63c67a6b924c98aaad29b83fe4
gyasi-eco <gcalhoun@twilio.com>
2023-04-24 17:03:09 -0400

    first attempt at fixing newline issue from message bodies

--- packages/dev-phone-ui/src/components/SendSmsForm/SendSmsForm.jsx
+++ packages/dev-phone-ui/src/components/SendSmsForm/SendSmsForm.jsx
@@ -12,14 +12,49 @@ function SendSmsForm({ numberInUse }) {
   const destinationNumber = useSelector(state => state.destinationNumber)
 
   const conversationsClient = useContext(TwilioConversationsContext)
-  const {sendMessage, sendSms} = conversationsClient
+  const { sendMessage, sendSms } = conversationsClient
 
   const canSendMessages = useMemo(() => {
     return destinationNumber && destinationNumber.length > 6;
   }, [destinationNumber]);
 
+  const inputContainer = {
+    boxSizing: "border-box",
+    display: "flex",
+    width: "100%",
+    backgroundColor: "rgb(255, 255, 255)",
+    boxShadow: "0 0 0 1px #8891aa",
+    borderRadius: "5px",
+    transition: "box-shadow 100ms ease-in",
+    cursor: "text",
+    height: "36px"
+  }
+  const textAreaStyle = {
+    boxSizing: "border-box",
+    appearance: "none",
+    backgroundColor: "transparent",
+    border: "none",
+    borderRadius: "5px",
+    boxShadow: "none",
+    color: "inherit",
+    cursor: "auto",
+    display: "block",
+    fontFamily: "inherit",
+    fontSize: "0.875rem",
+    fontWeight: "500",
+    lineHeight: "1.25rem",
+    margin: "0",
+    outline: "none",
+    padding: "0.5rem 0.75rem",
+    resize: "none",
+    width: "100%",
+    whiteSpace: "pre-wrap",
+  }
+
+
   // Handles the UI state for sending messages
   const sendIt = async (e) => {
+    formatMessage(messageBody)
     e.preventDefault()
     if (canSendMessages) {
       sendSms(numberInUse, destinationNumber, messageBody);
@@ -39,7 +74,9 @@ function SendSmsForm({ numberInUse }) {
         <Label htmlFor="sendSmsBody" required>Message</Label>
         <Grid gutter={"space20"} marginBottom="space40">
           <Column span={10}>
-            <Input id="sendSmsBody" type="text" value={messageBody} onChange={(e) => setMessageBody(e.target.value)} />
+            <div style={inputContainer}>
+              <textarea style={textAreaStyle} id="sendSmsBody" type="text" value={messageBody} onChange={(e) => setMessageBody(e.target.value)} />
+            </div>
           </Column>
           <Column span={2}>
             <Button type={"submit"} disabled={!canSendMessages}>
