4010387d487b45ec11316f9e78a2562f7fbf82fd
gyasi-eco <gcalhoun@twilio.com>
2023-06-14 12:25:49 -0400

    revert from paste chatlog to custom messaging component

--- packages/dev-phone-ui/src/components/SendSmsForm/MessageList.jsx
+++ packages/dev-phone-ui/src/components/SendSmsForm/MessageList.jsx
@@ -1,13 +1,7 @@
-import {
-    Box, Flex, SkeletonLoader, 
-    Text, ChatLog, ChatMessage,
-    ChatBubble, ChatMessageMeta, ChatMessageMetaItem, 
-    Avatar
-} from "@twilio-paste/core"
-import { UserIcon } from '@twilio-paste/icons/esm/UserIcon';
+import { Box, Flex, SkeletonLoader, Text } from "@twilio-paste/core"
 import { useSelector } from "react-redux"
 import EmptyMessageList from "./EmptyMessageList";
-
+import MessageBubble from "./MessageBubble"
 
 function MessageList({ devPhoneName }) {
     const messageList = useSelector(state => state.messageList)
@@ -15,33 +9,20 @@ function MessageList({ devPhoneName }) {
 
     return (
         messageList ?
-            // Replace Box with Chat Log
-            <Box overflowY="scroll" height="size40" tabIndex={0}>
-                <ChatLog>
-                    {messageList.length > 0 ?
-                        messageList.map((message, i) => {
-                            const isFromDevPhone = message.author === devPhoneName;
-                            return (
-                                <ChatMessage variant={!isFromDevPhone ? "outbound" : "inbound"}>
-                                    <ChatBubble>
-                                        {message.body}
-                                    </ChatBubble>
-                                    <ChatMessageMeta aria-label="">
-                                        <ChatMessageMetaItem>
-                                            <Avatar size="sizeIcon30" name={message.author} icon={UserIcon} />
-                                            {message.author}
-                                        </ChatMessageMetaItem>
-                                    </ChatMessageMeta>
-
-                                </ChatMessage>
-                                // <MessageBubble message={message} key={message.sid} devPhoneName={devPhoneName} />
-                            )
-                        })
-                        : <EmptyMessageList devPhoneNumber={numberInUse} />
-                    }
-                </ChatLog>
-            </Box>
-            : <SkeletonLoader height={"size20"} />
+            <Box height={'size40'}>
+                <Flex vertical vAlignContent={"bottom"} height={"100%"}>
+                    <Box width={"100%"} paddingRight={"space100"} paddingBottom={"space40"} paddingLeft={"space40"} overflowY={'scroll'} overflowX={'hidden'}>
+                        {messageList.length > 0 ?
+                            messageList.map((message, i) => {
+                                return (
+                                    <MessageBubble message={message} key={message.sid} devPhoneName={devPhoneName} />
+                                )
+                            })
+                            : <EmptyMessageList devPhoneNumber={numberInUse} />
+                        }
+                    </Box>
+                </Flex>
+            </Box> : <SkeletonLoader height={"size20"} />
     )
 
 }
